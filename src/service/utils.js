d7d6372a1c759ccb8856ac876d9fc38ea0e1e345
Eyal <40611633+eyalBarGit@users.noreply.github.com>
2022-01-15 22:46:24 +0200

    feat/DEV-001-create-store

---
+++ src/service/utils.js
@@ -0,0 +1,35 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const final = Math.floor(Math.random() * (max - min + 1)) + min;
+  return final;
+}
+
+// function getRandomIntInclusive(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+// }
+function generatePassword(length) {
+  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let retVal = '';
+  for (let i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+function saveToStorage(key, val) {
+  localStorage.setItem(key, JSON.stringify(val));
+}
+
+function loadFromStorage(key) {
+  const val = localStorage.getItem(key);
+  return JSON.parse(val);
+}
+module.exports = {
+  generatePassword,
+  loadFromStorage,
+  getRandomIntInclusive,
+  saveToStorage,
+};
--- src/views/GamePage/GamePage.vue
+++ src/views/GamePage/GamePage.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="h-screen flex flex-col justify-between border-8 border-red-500 text-center md:px-20">
+  <div class="h-screen flex flex-col justify-between text-center md:px-20">
     <div class="text-5xl">Welcome to Game Page</div>
     <div
-        class="p-10 h-full flex flex-col items-center justify-center border border-green-200"
+        class="p-10 h-full flex flex-col items-center justify-center "
     >
-      <GameBoard class="h-full w-full game-board border border-green-200 flex  flex-grow-0 "/>
+      <GameBoard class="h-4/6 md:h-full w-full game-board flex  flex-grow-0 "/>
     </div>
     <div class="h-20 w-full"></div>
   </div>
@@ -15,7 +15,7 @@ const GameBoard = () => import('../../components/GameBoard/GameBoard.vue');
 
 export default {
   name: 'GamePage',
-  components: {GameBoard},
+  components: { GameBoard },
 };
 </script>
 
